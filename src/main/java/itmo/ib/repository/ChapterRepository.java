be22edc87c890163759986db3273e2cfcfcae8ac
Anastasia <nastenysh.pa@gmail.com>
2025-11-13 17:17:52 +0300

    chore: init github pages

--- src/main/java/itmo/ib/repository/ChapterRepository.java
+++
@@ -1,9 +0,0 @@
-package itmo.ib.repository;
-
-import itmo.ib.model.Chapter;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChapterRepository extends JpaRepository<Chapter, Long> {
-}
